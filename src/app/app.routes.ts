b23a9309d0d865478a4bfb45622603d994ec6d2a
unknown <wsaraivabarros@gmail.com>
2025-01-10 15:36:19 -0300

    adicionando router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: 'home', pathMatch:'full'},
+    {path:'home', component:HomeComponent},
+    {path:'contato', component:ContatoComponent},
+    {path:'curriculo', component:CurriculoComponent},
+];
